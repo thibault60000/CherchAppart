8654a340208c7db58f370e320cc353e2379018d9
Stan <sandujar@free.fr>
2017-10-16 15:07:21 +0200

    Update Back
    Mise à jours security.yml
    Delete des default (controller, views)
    Modification sur les redirection une fois logged

--- src/CherchAppartBundle/Controller/SecurityController.php
+++ src/CherchAppartBundle/Controller/SecurityController.php
@@ -13,9 +13,9 @@ class SecurityController extends Controller
 {
     public function loginAction()
     {
-
-        if ($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')){
-            $this->redirectToRoute('cherch_appart_homepage_logged');
+        if ($this->container->get('security.authorization_checker')->isGranted('ROLE_ADMINISTRATEUR'))
+        {
+            $this->redirectToRoute('home');
         }
 
         $auth= $this->get('security.authentication_utils');

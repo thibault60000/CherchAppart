f12ace702946f348c99241cae917fbc289ed4281
Thibault JP <thibault60000@gmail.com>
2017-10-22 13:49:18 +0200

    IMG modif profil

--- web/js/util.js
+++ web/js/util.js
@@ -597,3 +597,21 @@
 	};
 
 })(jQuery);
+
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            $('#blah').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#fos_user_profile_form_imageFile").change(function() {
+    readURL(this);
+});

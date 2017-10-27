7aeea42a5fe9ed64f9572a1b9f8d5f8263f18b3e
Thibault JP <thibault60000@gmail.com>
2017-10-27 10:57:18 +0200

    Design Edit Account

--- src/CherchAppartBundle/Form/ProfileType.php
+++ src/CherchAppartBundle/Form/ProfileType.php
@@ -2,6 +2,7 @@
 namespace CherchAppartBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -12,6 +13,8 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('username', TextType::class,  array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle', 'disabled' => 'disabled'))
+            ->add('email', EmailType::class,  array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle', 'disabled' => 'disabled'))
             ->add('imageFile', FileType::class,  array('label' => 'form.imageFile', 'translation_domain' => 'FOSUserBundle'))
             ->add('adress', TextType::class, array('label' => 'form.adress', 'translation_domain' => 'FOSUserBundle'))
             ->add('postal_code', NumberType::class, array('label' => 'form.postal_code', 'translation_domain' => 'FOSUserBundle'))

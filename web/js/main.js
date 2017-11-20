912f8eda8885fb461891181b05d185e6a2126fe1
thibault60000 <thibault60000@gmail.com>
2017-11-20 16:37:34 +0100

    Modal Perfect <3

--- web/js/main.js
+++ web/js/main.js
@@ -34,13 +34,40 @@ $(function() {
             $("header nav").removeClass("active");
             $("header > img").removeClass("active");
         }
+
+
     });
 
         var dialogElConnect = document.getElementById('my-accessible-dialog-connect');
         var mainEl = document.getElementById('main');
         var dialogConnect = new window.A11yDialog(dialogElConnect, mainEl);
 
+
+
         var dialogElRegister = document.getElementById('my-accessible-dialog-register');
         var dialogRegister = new window.A11yDialog(dialogElRegister, mainEl);
 
+
+    $('.btnModal').click(function(){
+        var attrModal = $('main').attr('aria-hidden');
+        if (attrModal == 'true'){
+            $('html').css('overflow','hidden');
+        }
+        else{
+            $('html').css('overflow','auto');
+        }
+    });
+
+
+    $('.dialog-overlay').click(function(){
+        var attrModal = $('main').attr('aria-hidden');
+        if (attrModal == 'true'){
+            $('html').css('overflow','hidden');
+        }
+        else{
+            $('html').css('overflow','auto');
+        }
+    });
+
 });
+
